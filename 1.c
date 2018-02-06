e0ea43d7840902e2a60c48717d2d3a19c9835bfc
Walter Wu <walterwuyan@gmail.com>
2018-02-06 18:54:37 +0000

    Better variable names and turn back

--- 1.c
+++ 1.c
@@ -11,43 +11,51 @@
 #include "ping.h"
 #include "basicmove.h"
 
+struct Log {
+    int ticksL;
+    int ticksR;
+    int speedL;
+    int speedR;
+};
+
+
 int main() {
-    int ticksL[199], ticksR[199], counter = 0, preLT = 0, preRT = 0;
-    int lastLeft = 0;
-    int newLeft = leftDis();
-    int preLS = 64, preRS = 64;
-    drive_speed(preLS, preRS);
+    Log logs[199];
+    int logCounter = 0, preLeftTicks = 0, preRightTicks = 0, preLeftSpeed = 64, preRightSpeed = 64;
+    int preLeftDis = 0;
+    int newLeftDis = leftDis();
+    drive_speed(preLeftSpeed, preRightSpeed);
     while (ping_cm(8) > 12) {
         int needRecord = 0;
         int newLS = 0, newRS = 0;
 
-        lastLeft = newLeft;
-        newLeft = leftDis();
+        preLeftDis = newLeftDis;
+        newLeftDis = leftDis();
 
-        int change = newLeft - lastLeft;
+        int leftDisChange = newLeftDis - preLeftDis;
 
-        if (change > 10) {
+        if (leftDisChange > 10) {
             newLS = 16;
             newRS = 64;
-        } else if (change > 5) {
+        } else if (leftDisChange > 5) {
             newLS = 24;
             newRS = 64;
-        } else if (change > 3) {
+        } else if (leftDisChange > 3) {
             newLS = 32;
             newRS = 64;
-        } else if (change > 0) {
+        } else if (leftDisChange > 0) {
             newLS = 48;
             newRS = 64;
-        } else if (change == 0) {
+        } else if (leftDisChange == 0) {
             newLS = 64;
             newRS = 64;
-        } else if (change > -3) {
+        } else if (leftDisChange > -3) {
             newLS = 64;
             newRS = 48;
-        } else if (change > -6) {
+        } else if (leftDisChange > -6) {
             newLS = 64;
             newRS = 32;
-        } else if (change > -11) {
+        } else if (leftDisChange > -11) {
             newLS = 64;
             newRS = 24;
         } else {
@@ -55,26 +63,24 @@ int main() {
             newRS = 16;
         }
 
-        if (preLS != newLS || preRS != newRS) { // need to update speed and record
-            preLS = newLS;
-            preRS = newRS;
-            drive_speed(preLS, preRS);
-            drive_getTicks(&ticksL[counter], &ticksR[counter]);
-            //printf("before calc: %d %d\n", ticksL[counter], ticksR[counter]);
+        if (preLeftSpeed != newLS || preRightSpeed != newRS) { // need to update speed and record
+            preLeftSpeed = newLS;
+            preRightSpeed = newRS;
+            drive_speed(preLeftSpeed, preRightSpeed);
+            logs[logCounter].speedL = preLeftSpeed;
+            logs[logCounter].speedR = preRightSpeed;
+            drive_getTicks(&logs.ticksL[logCounter], &logs.ticksR[logCounter]);
             // calculate the difference in ticks
-            ticksL[counter] = ticksL[counter] - preLT;
-            ticksR[counter] = ticksR[counter] - preRT;
+            logs.ticksL[logCounter] = logs.ticksL[logCounter] - preLeftTicks;
+            logs.ticksR[logCounter] = logs.ticksR[logCounter] - preRightTicks;
             // update pre-ticks
-            preLT = preLT + ticksL[counter];
-            preRT = preRT + ticksR[counter];
-            //printf("after calc: %d %d\n", ticksL[counter], ticksR[counter]);
-            counter++;
+            preLeftTicks = preLeftTicks + logs.ticksL[logCounter];
+            preRightTicks = preRightTicks + logs.ticksR[logCounter];
+            logCounter++;
         }
+
         pause(50);
     }
-//    for (int i = 0; i < counter; i++) {
-//        printf("%d %d\n", ticksL[i], ticksR[i]);
-//    }
 
     // calculate position
     double x = 0.0;
@@ -82,30 +88,33 @@ int main() {
     double theta = 0.0;
     double BOT_WIDTH = 32.5538;
 
-    for (int i = 0; i < counter; i++) {
-        double currentD = (ticksL[i] - ticksR[i]) / BOT_WIDTH;
+    for (int i = 0; i < logCounter; i++) {
+        double currentD = (logs.ticksL[i] - logs.ticksR[i]) / BOT_WIDTH;
         theta = theta + currentD;
         if (theta != 0) {
-            double rl = ticksL[i] / theta;
-            double rr = ticksR[i] / theta;
+            double rl = logs.ticksL[i] / theta;
+            double rr = logs.ticksR[i] / theta;
             double rm = (rl + rr) / 2.0;
             x = x + rm - rm * cos(theta);
             y = y + rm * sin(theta);
         } else {
-            x = x + ticksL[i];
-            y = y + ticksR[i];
+            x = x + logs.ticksL[i];
+            y = y + logs.ticksR[i];
         }
 
     }
-    //printf("%f %f", x, y);
     x = x * 0.325;
     y = y * 0.325;
     double distance = sqrt(x * x + y * y);
     printf("Degree: %f radius, Distance: %f cm\n", theta, distance);
 
-
-    // Stop
+    // Stop, turn and pause
     drive_speed(0, 0);
+    drive_goto(51, -51);
+    pause(200);
+
+    // go back
+
 
     return 0;
 }
