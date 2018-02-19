60a9e63d5cdb1793a7f6cba9e43dd0d8d08f3282
Walter Wu <walterwuyan@gmail.com>
2018-02-19 16:13:11 +0000

    Changed leftDis and rightDis function

--- basicmove.h
+++ basicmove.h
@@ -46,12 +46,7 @@ float leftDis() {
         freqout(11, 1, 38000);
         init3 += input(10);
     }
-    for (int dacVal = 0; dacVal < 160; dacVal += 4) {
-        dac_ctr(26, 0, dacVal);
-        freqout(11, 1, 38000);
-        init4 += input(10);
-    }
-    return (init1 + init2 + init3 + init4) / 4.0;
+    return (init1 + init2 + init3) / 3.0;
 }
 
 float rightDis() {
@@ -71,12 +66,7 @@ float rightDis() {
         freqout(1, 1, 38000);
         init3 += input(2);
     }
-    for (int dacVal = 0; dacVal < 160; dacVal += 4) {
-        dac_ctr(27, 1, dacVal);
-        freqout(1, 1, 38000);
-        init4 += input(2);
-    }
-    return (init1 + init2 + init3 + init4) / 4.0;
+    return (init1 + init2 + init3) / 3.0;
 }
 
 #endif //PROJECT_BASICMOVE_H
